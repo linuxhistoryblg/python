92b9e9f65beb434381a95497c2e597cd8a86129a
dan <davis.dan.c@gmail.com>
2020-04-24 11:17:31 -0400

    cleaned up imports

--- makeafile.py
+++ makeafile.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
-import subprocess,sys
+from subprocess import run
+from sys import argv
+from sys import exit
 ''' Demonstrate command line args passed to python when using
     if __name__=="__main__" construction. Call makefile.py <filename>
     at the command line to create an empty file with name <filename>.
 '''
 
 def makefile(file):
-    subprocess.run(['touch',file])
+    run(['touch',file])
 
 if __name__ == "__main__":
-    makefile(sys.argv[1])
+    '''touch the file given as the first argument, if given
+       if argv < 1, return usage statement. A more sophisticated
+       handling of arguments could be implemented with argpase
+    '''
+    try:
+        makefile(argv[1])
+    except IndexError:
+        print('Usage: makeafile <filename>')
+        exit(69)
